b162a2e250a3c697a03d2803f896037e2e6c9bee
Holly Ullyott <ullyott@tinsley.jb.man.ac.uk>
2025-09-14 18:48:25 +0100

    works fine but slow

--- codes/make_master_cat.py
+++ codes/make_master_cat.py
@@ -11,6 +11,7 @@ import time
 import numpy as np
 from datetime import datetime
 import matplotlib.pyplot as plt
+from itertools import combinations
 from matplotlib.patches import Circle
 from astropy.table import Table, hstack, join
 from astropy.coordinates import SkyCoord, search_around_sky
@@ -178,17 +179,18 @@ def make_small_test_cat(filters, size=50):
         shortCat.write(combinedCatPath.replace(".fits", "_short.fits"), overwrite=True)
 
 # match by RA/Dec with maximum 1as seperation 
-def match_objs(maxsep, testing=True, verbose=False):
+def find_nearest(maxsep, size, testing=True, verbose=False):
     """ 
-    maxsep (arcsec) maximum allowed seperation across different band images (in arcseconds) to be considered the same object 
+    maxsep(int/float) Maximum allowed seperation across different band images (in arcseconds) to be considered the same object 
     """
 
     import astropy.units as u
     from scipy.spatial import cKDTree # works in Cartesian, https://youtu.be/TLxWtXEbtFE
     from astropy.coordinates import SkyCoord, search_around_sky, match_coordinates_sky # couldn't get s_a_s nor m_c_s to work # TODO: look at source code to see if/how Astropy make their search robust against missed nearest neighbours
     
-    if testing :
+    if testing:
         combinedCatPath = os.path.join(globalCatDir, "combined_cat_short.fits") #TODO: "combined_cat.fits" full version
+        #combinedCatPath = os.path.join(globalCatDir, "YJ_100Matches.fits") #TODO: "combined_cat.fits" full version
     else:
         combinedCatPath = os.path.join(globalCatDir, "combined_cat.fits") #TODO: "combined_cat.fits" full version
     
@@ -215,15 +217,10 @@ def match_objs(maxsep, testing=True, verbose=False):
         #coordsDict[filt] = SkyCoord(ra=ra * u.deg, dec=dec * u.deg) # for search_around_sky attempt
         coordsDict[filt] = np.array([ra, dec])                       # for manual attempt
 
-    nnDict = {}
-    for i, df in enumerate(detectionFilters[:-1]):
-        coords1 = coordsDict[df]
-        if detectionFilters[i+1] in coordsDict:
-            coords2 = coordsDict[detectionFilters[i+1]]
-
-        if coords1 is None or coords2 is None:
-            print(f"Skipping {df} vs {detectionFilters[i+1]} (no valid coords)")
-            continue
+    nnIDdict = {}
+    for df1, df2 in combinations(detectionFilters, 2):  # compare all unique pairs
+        coords1 = np.array(coordsDict[df1]).T # transopse to shape (N, 2) for cKDTree
+        coords2 = np.array(coordsDict[df2]).T# shape (M, 2)
 
         """
         # search_around_sky attempt
@@ -231,79 +228,142 @@ def match_objs(maxsep, testing=True, verbose=False):
         seplimit = 1 * u.arcsec # maximum allowed seperation to be considered the same object
         idx1, idx2, sep2d, _ = coords1.search_around_sky(coords2, seplimit)
         """
- 
-        # manual attempt
-        # Build KDTree for catalog 1
-        tree1 = cKDTree(coords1)
-
-        # Query nearest neighbor in cat1 for each point in cat2
-        k = len(detectionFilters)
-        distances, indices = tree1.query(coords2, k=k) #TODO: when using full-size cat, need to increase k (no of NN to return)
-        idx1, idx2 = indices # idx1 indeces of NN in cat1
-        nnDict[df] = idx1
-        nnDict[detectionFilters[i+1]] = idx2
+        k = int(size/10)+1#100 # TODO: how many NNs do you want to check? 
+        # All of them, but we need to plot fewer than this in nn_plotter
+        # TODO: when using full-size cat, need to increase k (no of NN to return)
         # k = Either number of NN to return, or list of k-th NN to return, starting from 1.
+        # This means the first index is the point itself, with a distance 0.
+
+        # manual attempt 
+        tree1 = cKDTree(coords1)
+        tree2 = cKDTree(coords2)
+
+        # Query nearest neighbor in cat1
+        distances1, indices1 = tree1.query(coords2, k=k) # indices1: for each point in coords2, the index of the nearest neighbor(s) in coords1.
+        distances2, indices2 = tree2.query(coords1, k=k) # indices2: for each in coords1, the index of NN in coords2.
+        nnIDdict[df2] = indices1  # coords2 -> coords1
+        nnIDdict[df1] = indices2  # coords1 -> coords2
+
         # distances to the nearest neighbors, indecies of the NNs in the cats
         # Missing neighbors are indicated with infinite distances. Hits are sorted by distance (nearest first).
 
-        print(f"Found nearest neighbours in filters {df} and {detectionFilters[i+1]}... ")
+        #print(f"Found nearest neighbours in filters {df1} and {df2}... ")
 
-    ### Do visual checks of nearest neighbours
-    # plot each of the nearest neightbours across the bands one one image. draw a max-sep circle to visually verify they are the same object
+    return nnIDdict, coordsDict
 
-    maxsep_dimless = maxsep/3600 # 1 arcsec
+def nn_plotter(coordsDict, nnIDdict, maxsep, testing=True):
+    """
+    Plots a circle of radius maxsep around each of the points provided. 
+    Provided points should be the nearest neighbours found by KDTree.
+
+    coordsDict{dict}    All coordinates in the combined (or test) catalogue for each of the detection filters {filt: Ra, Dec}
+    nnIDdict{dict}      Indices of the nearest neighbours found by KDTree {filt: [idx]}
+    maxsep(int/float)   Maximum allowed seperation across different band images (in arcseconds) to be considered the same object 
+    """
+    maxsep = maxsep / 3600
 
     fig, ax = plt.subplots(figsize=(6, 6))
-    cm = plt.cm.get_cmap("rainbow") # TODO double check filt correspond to colour: short -> long wl
 
-    # Set limits and equal aspect before adding circles
-    ax.set_xlim(min(coordsDict[df][1][nnDict[df]]) - 0.5, max(coordsDict[df][1][nnDict[df]]) + 0.5)
-    ax.set_ylim(min(coordsDict[df][0][nnDict[df]]) - 0.5,max(coordsDict[df][0][nnDict[df]]).max() + 0.5)
-    ax.set_aspect('equal', adjustable='datalim')  # ensures 1:1 scaling
-    a =  len(detectionFilters)
+    for df1, df2 in combinations(detectionFilters, 2):  # compare all unique pairs
 
-    for i, df in enumerate(detectionFilters[:-1]):
+        # Set limits and equal aspect before adding circles
+        # x-axis = RA, y-axis = Dec
+        ax.set_xlim(min(coordsDict[df1][0]) - 0.1, max(coordsDict[df1][0]) + 0.1)
+        ax.set_ylim(min(coordsDict[df1][1]) - 0.1, max(coordsDict[df1][1]) + 0.1)
+        ax.set_aspect('equal', adjustable='datalim')  # ensures 1:1 scaling
+
+        # rename for readability
+        df1_ra  = coordsDict[df1][0][nnIDdict[df1]]  # all NN RA for  filt 1
+        df1_dec = coordsDict[df1][1][nnIDdict[df1]]  # all NN Dec for filt 1
+
+        # draw circles around all points
+        for NNra, NNdec in zip(df1_ra, df1_dec):
+            print('circles...................')
+            for indiRA, indiDec in zip(NNra, NNdec):
+                validation_circle = Circle((indiRA, indiDec),
+                    maxsep, fill=False, edgecolor='magenta', linewidth=1)
+                ax.add_patch(validation_circle)
+
+        df2 = detectionFilters[-1:] # let's get that last pesky filter yoloswag
+        df2 = df2[0]
+        df2_ra  = coordsDict[df2][0][nnIDdict[df2]]  # all NN RA for  filt 2
+        df2_dec = coordsDict[df2][1][nnIDdict[df2]]  # all NN Dec for filt 2
+
+        for NNra, NNdec in zip(df2_ra, df2_dec):
+            print('squares...................')
+            for indiRA, indiDec in zip(NNra, NNdec):
+                validation_circle = Circle((indiRA, indiDec),
+                    maxsep, fill=False, edgecolor='magenta', linewidth=1)
+                ax.add_patch(validation_circle)
+
+    for df in detectionFilters:
+        # colours for plot
+        cm = plt.colormaps.get_cmap("rainbow")
+        a =  len(detectionFilters)
+        i = detectionFilters.index(df)
         colour = cm(i/a)
-        # plot the scatter points
-        ax.scatter(coordsDict[df][1][nnDict[df]], coordsDict[df][0][nnDict[df]], marker='.', color=colour, label=df)
 
-        # loop over *all* matched indices
-        for j in nnDict[df]:
-            validation_circle = Circle(
-                (coordsDict[df][1][j], coordsDict[df][0][j]),
-                maxsep_dimless, fill=False, edgecolor='magenta', linewidth=1)
-            ax.add_patch(validation_circle)
+        ## plot the scatter points
+        df_ra = coordsDict[df][0][nnIDdict[df]]
+        df_dec = coordsDict[df][1][nnIDdict[df]]
+        ax.scatter(df_ra, df_dec, marker='.', color=colour, label=df)
 
-    if detectionFilters[i+1] in coordsDict:
-        colour = cm(i/a)
-        df = detectionFilters[i+1]
-        validation_circle = Circle(
-                (coordsDict[df][1][j], coordsDict[df][0][j]),
-                maxsep_dimless, fill=False, edgecolor='magenta', linewidth=1)
-        ax.add_patch(validation_circle)
-
-    ax.set_xlabel("Dec (deg)")
-    ax.set_ylabel("RA (deg)")
-    plt.title(f"Nearest Neighbours")
+    ax.set_xlabel("RA (deg)")
+    ax.set_ylabel("Dec (deg)")
+    if testing:
+        plt.title(f"Nearest Neighbours to each det for middle {size} rows")
+        print(">>>>>> NOTE: for small test catalogues, detections will appear as strips since all detections are takens from rows in the main catalogue.")
+    else:
+        plt.title(f"Nearest Neighbours to each det")
     plt.legend()
     plt.show()
-       
 
-    '''
-    # cKDTree - class provides an index into a set of k-dimensional points which can be used to quickly look-up the nearest neighbors of any point
-    # https://youtu.be/TLxWtXEbtFE
-    # is only an approximate technique - some nearest neightbours may be missed
-    tree1 = cKDTree(xyz1) # ra/dec space of points split by the median along each axis into quadrants
-    tree2 = cKDTree(xyz2) # th nearest neighbour (NN) in each quadrat is assigned as NN - not perfect, but fast
+def show_same_objs(nnIDdict, coordsDict, maxsep):
+    """
+    Plots the nearest neighbours that have been validated as being the same object in detection bands
+    """
 
-    # Find all pairs within a given angular separation
-    maxsep = 1 * u.arcsec # maximum allowed seperation to be considered the same object
-    #r = 2 * np.sin(0.5 * maxsep.to_value(u.rad)) # assuming the sky is a sphere (in radians)
-    r = maxsep # assuming the sky is flat
-    pairs = tree1.query_ball_tree(tree2, r)'''
+    ### now match the NNs across the detFilts to verify whether it's the same objects detected in diff filters
+    # sameObjs: filt:coords, separation
+    # filts in which it's detected,  
+    sameObjs = {} 
+    nnDict = {} # df: NN_Ra, NN_Dec
 
+    for df in nnIDdict:
+        nnDict[df] = coordsDict[df][0][nnIDdict[df]], coordsDict[df][1][nnIDdict[df]]  # ra, dec
 
-  
+    #compare coord pairs
+
+    '''for df in nnDict:
+        sameObjs[df] = [] # start each key as an empty list so we don't overwrite coord entries
+        for detfilt in nnDict:
+            sameObjs[detfilt] = [] # start each key as an empty list
+            for j in range(len(nnDict[df][0])):
+                ra1, dec1 = nnDict[df][0][j], nnDict[df][1][j]
+                ra2, dec2 = nnDict[detfilt][0][j], nnDict[detfilt][1][j]
+                separation_deg = np.sqrt(abs(ra1 - ra2)**2 + abs(dec1 - dec2)**2)
+                sep_as = separation_deg * 3600
+
+                if sep_as <= maxsep:
+                    sameObjs[df].append((detfilt, ra1, dec1, sep_as))
+                    #sameObjs[detfilt].append((ra2, dec2, sep_as))
+                else:
+                    pass'''
+
+    for df, detfilt in itertools.combinations(nnDict.keys(), 2):
+        sameObjs[df] = []
+        for j in range(len(nnDict[df][0])):
+            ra1, dec1 = nnDict[df][0][j], nnDict[df][1][j]
+            ra2, dec2 = nnDict[detfilt][0][j], nnDict[detfilt][1][j]
+            separation_deg = np.sqrt(abs(ra1 - ra2)**2 + abs(dec1 - dec2)**2)
+            sep_as = separation_deg * 3600
+
+            if sep_as <= maxsep:
+                sameObjs[df].append((detfilt, ra1, dec1, sep_as))
+            else:
+                pass
+
+    print(sameObjs)
 
 
 #####################################################################################################################################
@@ -312,10 +372,26 @@ def match_objs(maxsep, testing=True, verbose=False):
 #modify colnames to preserve detFilt_measFilt
 #run_parallel(modify_colnames, detectionFilters, max_workers=len(detectionFilters), use_processes=False)
 #join_cats()
-#make_small_test_cat(detectionFilters, size=500)
-match_objs(1)
+size=100
+#make_small_test_cat(detectionFilters, size=size)
+
+maxsep = 100 # as
+nnIDdict, coordsDict = find_nearest(maxsep, size)
+nn_plotter(coordsDict, nnIDdict, maxsep)
+show_same_objs(nnIDdict, coordsDict, maxsep)
 #run_parallel(func, input, max_workers=len(detectionFilters), use_processes=False)
 
+'''
+# cKDTree - class provides an index into a set of k-dimensional points which can be used to quickly look-up the nearest neighbors of any point
+# https://youtu.be/TLxWtXEbtFE
+# is only an approximate technique - some nearest neightbours may be missed
+tree1 = cKDTree(xyz1) # ra/dec space of points split by the median along each axis into quadrants
+tree2 = cKDTree(xyz2) # th nearest neighbour (NN) in each quadrat is assigned as NN - not perfect, but fast
 
+# Find all pairs within a given angular separation
+maxsep = 1 * u.arcsec # maximum allowed seperation to be considered the same object
+#r = 2 * np.sin(0.5 * maxsep.to_value(u.rad)) # assuming the sky is a sphere (in radians)
+r = maxsep # assuming the sky is flat
+pairs = tree1.query_ball_tree(tree2, r)'''
 
 
